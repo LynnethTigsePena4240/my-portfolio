1edf76545a2ac2d762cb17b7b5080b37b1ad1697
lynneth <lynneth_tigsepena@hotmail.com>
2025-02-28 21:48:51 -0500

    added functionality to the flex boxes for the projects so now they show text when the btn is clicked and if the btn i clicked again it hiddes the text again

---
+++ script.js
@@ -0,0 +1,48 @@
+
+var showbtn1 = document.getElementById("showbtn1")
+var showbtn2 = document.getElementById("showbtn2")
+var showbtn3 = document.getElementById("showbtn3")
+var display = document.getElementById("text1")
+
+showbtn1.addEventListener("click",function()
+{
+    var text = document.getElementById("text1")
+    if(text.style.display === "none")
+    {
+        text.style.display = "block"
+    }
+
+    else
+    {
+        text.style.display = "none"
+    }
+
+})
+
+showbtn2.addEventListener("click",function()
+{
+    var text = document.getElementById("text2")
+    if (text.style.display === "none")
+    {
+        text.style.display = "block"
+    }
+    
+    else
+    {
+         text.style.display = "none"
+    }
+})
+
+showbtn3.addEventListener("click",function()
+{
+    var text = document.getElementById("text3")
+    if (text.style.display === "none")
+    {
+        text.style.display = "block"
+    }
+        
+    else
+    {
+        text.style.display = "none"
+    }
+})
